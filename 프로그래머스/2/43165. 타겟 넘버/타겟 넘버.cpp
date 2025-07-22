cd83d0e4bb59f125d4046e54c123513c81d30e58
GeonHyeongLee <153305458+GeonHyeongLee@users.noreply.github.com>
2025-07-22 09:08:44 +0900

    [level 2] Title: 타겟 넘버, Time: 4.42 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -5,20 +5,21 @@ using namespace std;
 
 int answer = 0;
 
-void dfs(vector<int> &numbers, int cur_idx, int num, int idx_len, int target) {
-    if (cur_idx == idx_len) {
+void dfs(vector<int>& numbers, int cur_idx, int num, int all_idx, int target) {
+    if (cur_idx == all_idx) {
         if (num == target) {
             answer += 1;
         }
         return;
     }
     
-    dfs(numbers, cur_idx + 1, num + numbers[cur_idx], idx_len, target);
-    dfs(numbers, cur_idx + 1, num - numbers[cur_idx], idx_len, target);
+    dfs(numbers, cur_idx + 1, num + numbers[cur_idx], all_idx, target);
+    dfs(numbers, cur_idx + 1, num - numbers[cur_idx], all_idx, target);
 }
 
 int solution(vector<int> numbers, int target) {
     
     dfs(numbers, 0, 0, numbers.size(), target);
+    
     return answer;
 }
