52ea933172f51a0f2f3a2db85266c5f81d89eb3a
GeonHyeongLee <153305458+GeonHyeongLee@users.noreply.github.com>
2025-08-12 21:08:49 +0900

    [Gold V] Title: 치킨 배달, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -16,23 +16,74 @@ int graph[MAX][MAX];
 vector<pair<int,int>> houses;
 vector<pair<int,int>> chickens;
 
+int best = 987654321;
+
 vector<int> result;
 
-vector<vector<int>> choosing_chickens; // 조합이 담겨있다
+int city_chicken_dis(vector<int>& result) {
+	// result가 {0 , 1}이라고 한다면 방문할 치킨집은
+	// chickens[0]과 chickens[1]
+
+	// 집을 기준으로 찾아야하므로 집 먼저
+
+	int real_result = 0;
+
+	for (auto house : houses) {
+		int house_x = house.first;
+		int house_y = house.second;
+
+		int min_dist = 987654321;
+
+		for (int i = 0; i < result.size(); i++) {
+			int chicken_num = result[i];
+			int chicken_x = chickens[chicken_num].first;
+			int chicken_y = chickens[chicken_num].second;
+
+			int dist = abs(house_x - chicken_x) + abs(house_y - chicken_y);
+
+			min_dist = min(min_dist, dist);
+		}
+		real_result += min_dist;
+
+		// "추가" 이미 real_result가 best를 넘어섰다면 볼 필요 없을 것
+		if (real_result >= best) {
+			return real_result;
+		}
+	}
+
+	return real_result;
+}
 
 void find_permutation(int cur, int cnt) {
 	if (cnt == m) {
+		// "중요" 
+		// 조합을 진행하면서 뽑은 result를 그대로 거리를 구해서 최솟값을 
+		// 구해나갔다면 처음 했던 것처럼 vector<vector<int>>를 만들어서
+		// 다시 저장할 필요가 없었을 것이다.
+		best = min(best, city_chicken_dis(result));
+		return;
+	}
 
-		choosing_chickens.push_back(result);
+	// "추가" 
+	// chicken 개수에서 cur 즉 현재 보고 있는 치킨 번호만큼 뺐을 때
+	// m - cnt보다 작다, 즉 남은 치킨 번호 수가 채워야 될 치킨 번호 수보다
+	// 적다면 종료
+	if (chickens.size() - cur < m - cnt) {
 		return;
 	}
 
 	for (int i = cur; i < chickens.size(); i++) {
 		result.push_back(i);
-		find_permutation(i + 1, cnt + 1);
+		// 중요 : 조합은 현재 고른 인덱스 i의 다음부터만 고른다
+		// i + 1일 때는 (1,2), (1,3), (1,4) ...
+		// cur + 1일 때는 (1,1),(1,2),(1,3),(1,4),(2,1),(2,2) 
+		// 이런식으로 (2,1)과 같이 볼 필요 없는 것들도 보게되므로 시간 초과
+		find_permutation(i + 1, cnt + 1); 
+
 		result.pop_back();
 	}
 }
+
 
 int main(void) {
 	cin >> n >> m;
@@ -52,38 +103,6 @@ int main(void) {
 	// 치킨 조합 만들기
 	find_permutation(0,0);
 
-	int min_num = 987654321;
+	cout << best;
 
-
-	vector<int> real_result;
-
-
-	for (int i = 0; i < choosing_chickens.size(); i++) {
-		int min_chicken_dis = 0;
-		for (int k = 0; k < houses.size(); k++) {
-
-			int house_x = houses[k].first;
-			int house_y = houses[k].second;
-
-			int min_dis = 98765431;
-			for (int j = 0; j < choosing_chickens[i].size(); j++) {
-				int chicken_num = choosing_chickens[i][j];
-				int chicken_x = chickens[chicken_num].first;
-				int chicken_y = chickens[chicken_num].second;
-
-				int dis = abs(house_x - chicken_x) + abs(house_y - chicken_y);
-				min_dis = min(min_dis, dis);
-			}
-			min_chicken_dis += min_dis;
-		}
-		real_result.push_back(min_chicken_dis);
-	}
-
-	for (int i = 0; i < real_result.size(); i++) {
-		if (real_result[i] < min_num) {
-			min_num = real_result[i];
-		}
-	}
-
-	cout << min_num;
 }
