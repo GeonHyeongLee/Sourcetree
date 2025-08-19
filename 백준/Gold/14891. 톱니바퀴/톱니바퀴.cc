7d7e4ebfd10bf5ceec12a94e30a0b24f748c5e5e
GeonHyeongLee <153305458+GeonHyeongLee@users.noreply.github.com>
2025-08-19 22:03:38 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -1,41 +1,86 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
+#define MAX 8
 
-vector<int> gears[4];
+int gears[MAX][MAX];
 
-void move_gear(int num, int dir) {
-	vector<int> gear_copy;
+void move_gear(int gear_num, int dir) {
 
-	for (int i = 0; i < gears[num].size(); i++) {
-		gear_copy.push_back(gears[num][i]);
+	int gear_copy[MAX];
+
+	for (int i = 0; i < 8; i++) {
+		gear_copy[i] = gears[gear_num][i];
 	}
 
-	// 시계 반시계 헷갈리지 않도록 하자 헷갈리면 {0,1,2,3,4,5,6,7} 기준으로 해볼 것
 	if (dir == 1) {
-		gears[num][0] = gear_copy[7];
+		// {0,1,2,3,4,5,6,7} => {7,0,1,2,3,4,5,6}
+		gears[gear_num][0] = gear_copy[7];
 		for (int i = 1; i <= 7; i++) {
-			gears[num][i] = gear_copy[i - 1];
+			gears[gear_num][i] = gear_copy[i - 1];
 		}
 	}
 	else if (dir == -1) {
-		gears[num][7] = gear_copy[0];
+		// {0,1,2,3,4,5,6,7} => {1,2,3,4,5,6,7,0}
+		gears[gear_num][7] = gear_copy[0];
 		for (int i = 0; i < 7; i++) {
-			gears[num][i] = gear_copy[i + 1];
+			gears[gear_num][i] = gear_copy[i + 1];
 		}
 	}
-	// 안 돌릴 때에는 dir == 0이므로 그냥 그대로 종료
 	else {
 		return;
 	}
 }
 
+int main(void) {
+	string gear;
+	for (int i = 0; i < 4; i++) {
+		cin >> gear;
+		for (int j = 0; j < 8; j++) {
+			gears[i][j] = (gear[j] - '0');
+		}
+	}
 
+	//for (int i = 0; i < 4; i++) {
+	//	for (int j = 0; j < 8; j++) {
+	//		cout << gears[i][j] << " ";
+	//	}
+	//	cout << "\n";
+	//}
 
-void calculate_result(int& result) {
+	int k;
+	cin >> k;
+
+	for (int i = 0; i < k; i++) {
+		int gear_num, dir;
+		cin >> gear_num >> dir;
+
+		gear_num -= 1;
+		int dirs[4] = { 0,0,0,0 };
+		dirs[gear_num] = dir;
+
+		// 기어 기준 오른쪽
+		for (int i = gear_num + 1; i < 4; i++) {
+			if (gears[i-1][2] != gears[i][6]) {
+				dirs[i] = -dirs[i-1];
+			}
+		}
+		// 기어 기준 왼쪽
+		for (int i = gear_num - 1; i >= 0; i--) {
+			if (gears[i+1][6] != gears[i][2]) {
+				dirs[i] = -dirs[i+1];
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			move_gear(i, dirs[i]);
+		}
+	}
+
+	int	result = 0;
+
 	if (gears[0][0] == 1) {
 		result += 1;
 	}
@@ -48,56 +93,6 @@ void calculate_result(int& result) {
 	if (gears[3][0] == 1) {
 		result += 8;
 	}
-}
 
-int main(void) {
-	string gear;
-	for (int i = 0; i < 4; i++) {
-		cin >> gear;
-		for (int j = 0; j < 8; j++) {
-			gears[i].push_back((gear[j] - '0'));
-		}
-	}
-
-	int k;
-	cin >> k;
-
-	for (int i = 0; i < k; i++) {
-		int num, dir;
-		cin >> num >> dir;
-		num = num - 1;
-
-		int dirs[4] = { 0,0,0,0 }; // 방향을 먼저 정해놓고 한 번에 돌리기 위해서 준비한다
-		dirs[num] = dir;
-
-		// gear 넘버 기준으로 왼쪽 기어가 어떻게 변할 지 판단
-		for (int i = num - 1; i >= 0; i--) {
-			if (gears[i][2] != gears[i+1][6]) {
-				dirs[i] = -dirs[i + 1];
-			}
-			else {
-				break;
-			}
-		}
-		// gear 넘버 기준으로 오른쪽 기어가 어떻게 변할 지 판단
-		for (int i = num + 1; i < 4; i++) {
-			if (gears[i - 1][2] != gears[i][6]) {
-				dirs[i] = -dirs[i - 1];
-			}
-			else {
-				break;
-			}
-		}
-
-		// 최종적으로 정해진 방향에 따라 모든 기어 회전
-		for (int i = 0; i < 4; i++) {
-			move_gear(i, dirs[i]);
-		}
-	}
-
-	// 회전된 기어의 값
-	int result = 0;
-	calculate_result(result);
-
-	cout << result;
+	cout << result << "\n";
 }
