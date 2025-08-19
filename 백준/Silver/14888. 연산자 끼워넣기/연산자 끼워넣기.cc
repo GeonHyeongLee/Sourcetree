59c9bd207016a0b3e906d8f0fdf07b2bb19b75e7
GeonHyeongLee <153305458+GeonHyeongLee@users.noreply.github.com>
2025-08-19 22:36:19 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,35 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
+
 using namespace std;
 
-// 연산자 모음 0 : 더하기, 1 : 빼기, 2 : 곱하기, 3 : 나누기
-vector<int> operators;
-
-// 연산자에 따른 계산
-long long int eval(vector<int>& nums, vector<int>& operators) {
-	long long int result_num = nums[0];
-
-	for (int i = 0; i < nums.size() - 1; i++) {
-		if (operators[i] == 0) {
-			result_num += nums[i + 1];
-		}
-		else if (operators[i] == 1) {
-			result_num -= nums[i + 1];
-		}
-		else if (operators[i] == 2) {
-			result_num *= nums[i + 1];
-		}
-		else if (operators[i] == 3) {
-			result_num /= nums[i + 1];
-		}
-	}
-	return result_num;
-}
-
 int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
 	int n;
 	cin >> n;
 	vector<int> nums;
@@ -43,8 +19,11 @@ int main(void) {
 	int minus;
 	int mul;
 	int div;
+
 	cin >> add >> minus >> mul >> div;
-	
+
+	vector<int> operators;
+
 	for (int i = 0; i < add; i++) {
 		operators.push_back(0);
 	}
@@ -60,20 +39,31 @@ int main(void) {
 
 	sort(operators.begin(), operators.end());
 
-	// next_permutation 사용하여서 전체 경우의 수 구하기
-	// do-while문을 사용하여야 한다.
-	long long max_num = -1000000001;
-	long long min_num = 1000000001;
+	long long int max_num = -1000000001;
+	long long int min_num = 1000000001;
 
 	do {
-		long long int result_num = eval(nums, operators);
+		long long int result = nums[0];
 
-		max_num = max(max_num, result_num);
-		min_num = min(min_num, result_num);
+		for (int i = 0; i < n - 1; i++) {
+			if (operators[i] == 0) {
+				result += nums[i + 1];
+			}
+			else if (operators[i] == 1) {
+				result -= nums[i + 1];
+			}
+			else if (operators[i] == 2) {
+				result *= nums[i + 1];
+			}
+			else if (operators[i] == 3) {
+				result /= nums[i + 1];
+			}
+		}
+
+		max_num = max(max_num, result);
+		min_num = min(min_num, result);
 
 	} while (next_permutation(operators.begin(), operators.end()));
-	
 
-	cout << max_num << "\n";
-	cout << min_num;
+	cout << max_num << "\n" << min_num << "\n";
 }
