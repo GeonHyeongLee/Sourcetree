0dc3a8cc68bd2cc8960402658dbe981158175eb9
GeonHyeongLee <153305458+GeonHyeongLee@users.noreply.github.com>
2025-08-23 15:30:20 +0900

    [Gold V] Title: 토마토, Time: 236 ms, Memory: 12248 KB -BaekjoonHub

--- C++17/백준/Gold/7569. 토마토/토마토.cc
+++ C++17/백준/Gold/7569. 토마토/토마토.cc
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <queue>
-#include <vector>
 #include <tuple>
+#include <vector>
+#include <queue>
 #include <algorithm>
+
 
 using namespace std;
 
 
-#define MAX 101
-int m; // 열
-int n; // 행
+int m; // 행
+int n; // 열
 int h; // 높이
 
 vector<vector<vector<int>>> graph(101, vector<vector<int>>(101, vector<int>(101)));
 vector<vector<vector<int>>> visited_day(101, vector<vector<int>>(101, vector<int>(101, -1)));
+
 
 int dx[6] = { 0,0,0,0,-1,1 };
 int dy[6] = { 0,0,-1,1,0,0 };
@@ -24,12 +25,10 @@ int main(void) {
 
 	queue<tuple<int, int, int>> q;
 
-
-	// 높이 => 행 => 열 순으로 입력 받기
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < n; j++) {
 			for (int k = 0; k < m; k++) {
-				cin >> graph[i][j][k]; 
+				cin >> graph[i][j][k];
 				if (graph[i][j][k] == 1) {
 					visited_day[i][j][k] = 0;
 					q.push({ i,j,k });
@@ -40,7 +39,6 @@ int main(void) {
 
 	while (!q.empty()) {
 		auto cur = q.front(); q.pop();
-		// tuple에서는 q.first, seocnd가 아닌 get<0>(cur) 이런식으로 출력
 		int z = get<0>(cur);
 		int x = get<1>(cur);
 		int y = get<2>(cur);
@@ -53,26 +51,21 @@ int main(void) {
 			if (nz < 0 || nz >= h || nx < 0 || nx >= n || ny < 0 || ny >= m) {
 				continue;
 			}
-			
-			// -1로 토마토가 들어있지 않은 점은 방문하지 않는다.
-			if (graph[nz][nx][ny] == -1) {
-				continue;
-			}
-			
-			// 방문 했으면 재방문 안한다.
+
 			if (visited_day[nz][nx][ny] != -1) {
 				continue;
 			}
-			// 토마토가 익지 않은 점을 방문하여서
-			// visited_day는 그전보다 +1, 익었으니까 graph의 해당 지점은 1로 바뀜
-			// 이후 q.push하여 해당 위치를 q에 저장
+
+			if (graph[nz][nx][ny] == -1) {
+				continue;
+			}
+
 			if (graph[nz][nx][ny] == 0) {
-				visited_day[nz][nx][ny] = visited_day[z][x][y] + 1;
 				graph[nz][nx][ny] = 1;
+				visited_day[nz][nx][ny] = visited_day[z][x][y] + 1;
 				q.push({ nz,nx,ny });
 			}
 		}
-
 	}
 
 	int max_result = 0;
@@ -91,19 +84,4 @@ int main(void) {
 	}
 
 	cout << max_result << "\n";
-
-	/*cout << "\n\n";
-
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < n; j++) {
-			for (int k = 0; k < m; k++) {
-				cout << v[i][j][k] << " ";
-			}
-			cout << "\n";
-		}
-		cout << "\n";
-	}*/
-
-
-
 }
