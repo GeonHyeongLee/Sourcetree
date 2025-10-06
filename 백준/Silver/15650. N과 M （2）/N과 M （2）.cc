85163cdb94282f5747cf732635cd673d09b1798d
GeonHyeongLee <153305458+GeonHyeongLee@users.noreply.github.com>
2025-10-06 17:58:06 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,34 +1,34 @@
 #include <iostream>
 #include <vector>
+
+
 using namespace std;
 
-#define MAX 9
+int N;
+int M;
 
-int N, M;
+vector<int> v;
+vector<int> res;
 
-vector<int> num_arr;
-vector<int> result;
-
-
-void print_num(void) {
-	for (int i = 0; i < result.size(); i++) {
-		cout << result[i] << " ";
+void print_res() {
+	for (int i = 0; i < res.size(); i++) {
+		cout << res[i] << " ";
 	}
 	cout << "\n";
 }
 
-void back_tracking(int cur , int count) {
-	if (count == M) {
-		print_num();
+void back_tracking(int cur_num, int cur_cnt, int len) {
+	if (cur_cnt == M) {
+		print_res();
 		return;
 	}
 
-	for (int i = 0; i < N; i++) {
-		if (num_arr[i] >= cur) {
-			cur = num_arr[i];
-			result.push_back(num_arr[i]);
-			back_tracking(cur + 1, count + 1);
-			result.pop_back();
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i] >= cur_num) {
+			cur_num = v[i];
+			res.push_back(cur_num);
+			back_tracking(v[i]+1, cur_cnt + 1, len);
+			res.pop_back();
 		}
 	}
 }
@@ -37,9 +37,9 @@ int main(void) {
 	cin >> N >> M;
 
 	for (int i = 1; i <= N; i++) {
-		num_arr.push_back(i);
+		v.push_back(i);
 	}
 
-	back_tracking(1, 0);
-
+	back_tracking(v[0], 0, M);
+	
 }
