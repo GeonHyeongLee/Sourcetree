8ec50a9c36a66ed4444dbe84ed80583ccbb914a5
GeonHyeongLee <153305458+GeonHyeongLee@users.noreply.github.com>
2025-10-15 21:40:20 +0900

    [Gold III] Title: 앱, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -3,43 +3,46 @@
 using namespace std;
 
 int n;
-long long int m;
+int m;
+
+
 int main(void) {
 	cin >> n >> m;
 
+	// 메모리 = 무게
 	vector<int> memory(n + 1);
+	
+	// 비용 = 가중치
 	vector<int> cost(n + 1);
-
-	int all_cost = 0;
 
 	for (int i = 1; i <= n; i++) {
 		cin >> memory[i];
 	}
+
+	int all_cost = 0;
+
 	for (int i = 1; i <= n; i++) {
 		cin >> cost[i];
 		all_cost += cost[i];
 	}
 
-	// cost 당 채울 수 있는 최대 메모리 수
-	vector<long long int> app_cost(all_cost + 1, 0);
+	vector<int> max_cost_memory(all_cost + 1, 0);
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = all_cost; j >= cost[i]; j--) {
-			app_cost[j] = max(app_cost[j], app_cost[j - cost[i]] + memory[i]);
+			max_cost_memory[j] = max(max_cost_memory[j], max_cost_memory[j - cost[i]] + memory[i]);
 		}
 	}
 
-	//for (int i = 0; i <= all_cost; i++) {
-	//	cout << app_cost[i] << " ";
-	//}
+	int result;
 
-	int result = 0;
 	for (int i = 0; i <= all_cost; i++) {
-		if (app_cost[i] >= m) {
+		if (max_cost_memory[i] >= m) {
 			result = i;
 			break;
 		}
 	}
+
 	cout << result;
 
 }
