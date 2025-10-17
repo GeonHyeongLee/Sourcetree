a75edd89ce8f6032b15e25baf9bc0d3f2c5ecd97
GeonHyeongLee <153305458+GeonHyeongLee@users.noreply.github.com>
2025-10-17 11:56:14 +0900

    [Gold IV] Title: 동전 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int n;
@@ -7,22 +8,20 @@ int k;
 
 int main(void) {
 	cin >> n >> k;
-
 	vector<int> coin(n + 1);
 
 	for (int i = 1; i <= n; i++) {
 		cin >> coin[i];
 	}
 
-	vector<int> cnt(k + 1, 0);
-
-	cnt[0] = 1; // 젤 중요 아무 동전도 선택하지 않았을 때
+	vector<int> dp(k + 1, 0);
+	dp[0] = 1;
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = coin[i]; j <= k; j++) {
-			cnt[j] += cnt[j - coin[i]];
+			dp[j] = max(dp[j], dp[j - coin[i]] + dp[j]);
 		}
 	}
 
-	cout << cnt[k];
+	cout << dp[k];
 }
