96067ceb44eb82416475fcee3263c549ead6bb9c
GeonHyeongLee <153305458+GeonHyeongLee@users.noreply.github.com>
2025-10-29 18:37:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,33 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <queue>
 #include <algorithm>
+#define MAX 26
 using namespace std;
 
-#define MAX 30
-
 int n;
+int cnt = 0;
 
+bool visited[MAX][MAX];
 int graph[MAX][MAX];
-int visited[MAX][MAX];
 
-int dx[4] = {0,0,-1,1};
-int dy[4] = {-1,1,0,0};
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { -1,1,0,0 };
 
-vector<int> result;
-
-void bfs(int x, int y, int& cnt) {
-
-	queue<pair<int, int>> q;
-	q.push({ x,y });
-	cnt += 1;
+void bfs(int x, int y, int& num) {
+	queue<pair<int,int>> q;
 	visited[x][y] = true;
+	q.push({ x,y });
 
 	while (!q.empty()) {
-		auto cur = q.front();
-		q.pop();
+		auto cur = q.front(); q.pop();
 		int cur_x = cur.first;
 		int cur_y = cur.second;
-		//cout << cur_x << " " << cur_y << "\n";
+
 		for (int i = 0; i < 4; i++) {
 			int nx = cur_x + dx[i];
 			int ny = cur_y + dy[i];
@@ -39,44 +36,54 @@ void bfs(int x, int y, int& cnt) {
 				continue;
 			}
 
+			q.push({ nx,ny });
 			visited[nx][ny] = true;
-			cnt += 1;
-			q.push({ nx, ny });
+			num++;
 		}
-
 	}
 
 }
 
 int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
 	cin >> n;
+	cin.ignore();
+
+
 	string s;
+
 	for (int i = 0; i < n; i++) {
 		cin >> s;
+
 		for (int j = 0; j < n; j++) {
-			graph[i][j] = (s[j] - '0');
+			int num = s[j] - '0';
+			graph[i][j] = num;
 		}
 	}
 
-	int town_cnt = 0;
+	//for (int i = 0; i < n; i++) {
+	//	for (int j = 0; j < n; j++) {
+	//		cout << graph[i][j] << " ";
+	//	}
+	//	cout << "\n";
+	//}
+
+	vector<int> apart_count;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (!visited[i][j] && graph[i][j] == 1) {
-				town_cnt += 1;
-				int cnt = 0;
-				bfs(i, j, cnt);
-				result.push_back(cnt);
+			if (visited[i][j] == 0 && graph[i][j] == 1) {
+				int apart_num = 1;
+				bfs(i, j, apart_num);
+				cnt++;
+				apart_count.push_back(apart_num);
 			}
 		}
 	}
+	sort(apart_count.begin(), apart_count.end());
 
-	cout << town_cnt << "\n";
+	cout << cnt << "\n";
 
-	sort(result.begin(), result.end());
-	for (int i = 0; i < result.size(); i++) {
-		cout << result[i] << "\n";
+	for (int i = 0; i < apart_count.size(); i++) {
+		cout << apart_count[i] << "\n";
 	}
 }
