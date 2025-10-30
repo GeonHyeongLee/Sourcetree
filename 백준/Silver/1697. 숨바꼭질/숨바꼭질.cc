15e19e7e9971b5077779919c34a91bb21cfb85ff
GeonHyeongLee <153305458+GeonHyeongLee@users.noreply.github.com>
2025-10-30 23:48:06 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2508 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,77 +1,37 @@
 #include <iostream>
 #include <queue>
 
-#define MAX 100001
-
 using namespace std;
 
+#define MAX 100001
+
 int n, k;
-// 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-// 1 0 1 0 0 
-// 1 2 
-// 5 -> (4,6,10) => (4,1),(6,1),(10,1)
-// (4,6,10) => (3,8),(7,12),(9,11,20) => (3,7,8,9,11,12,20 = 2)
-// 
+
 int result[MAX];
 bool visited[MAX];
-
-int min_num = 987654321;
-int cnt = 0;
 
 void bfs(int num) {
 	queue<int> q;
 	q.push(num);
 	visited[num] = true;
+
 	while (!q.empty()) {
 		int x = q.front(); q.pop();
-		
 		if (x == k) {
 			return;
 		}
+		int nxt_position[3] = {x + 1, x - 1,x * 2};
 
-		for (int i = 0; i < 3; i++) {
-			int nx;
-			if (i == 0) {
-				nx = x * 2;
-
-				if (nx < 0 || nx >= MAX) {
-					continue;
-				}
-
-				if (visited[nx]) {
-					continue;
-				}
-				q.push(nx);
-				visited[nx] = true;
-				result[nx] = result[x] + 1;
+		for (auto nx : nxt_position) {
+			if (nx < 0 || nx >= MAX) {
+				continue;
 			}
-			else if (i == 1) {
-				nx = x + 1;
-				if (nx < 0 || nx >= MAX) {
-					continue;
-				}
-
-				if (visited[nx]) {
-					continue;
-				}
-
-				q.push(nx);
-				visited[nx] = true;
-				result[nx] = result[x] + 1;
+			if (visited[nx]) {
+				continue;
 			}
-			else if (i == 2) {
-				nx = x - 1;
-				if (nx < 0 || nx >= MAX) {
-					continue;
-				}
-
-				if (visited[nx]) {
-					continue;
-				}
-				q.push(nx);
-				visited[nx] = true;
-				result[nx] = result[x] + 1;
-			}
+			q.push(nx);
+			visited[nx] = true;
+			result[nx] = result[x] + 1;
 		}
 	}
 }
@@ -80,10 +40,6 @@ int main(void) {
 	cin >> n >> k;
 
 	bfs(n);
-
-	//for (int i = 0; i <= 100; i++) {
-	//	cout << result[i] << "\n";
-	//}
 
 	cout << result[k];
 }
