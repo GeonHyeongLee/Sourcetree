c3f84a213ff086532cee083cb5ba5fa8bb0fd76e
GeonHyeongLee <153305458+GeonHyeongLee@users.noreply.github.com>
2025-11-19 10:20:01 +0900

    [Gold IV] Title: 타임머신, Time: 12 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -6,63 +6,64 @@
 
 using namespace std;
 
+int N;
+int M;
+
 struct Edge {
 	int from;
 	int to;
 	int cost;
 };
 
-int N, M;
-
 int main(void) {
-	cin >> N >> M; // N : 노드 개수, M : 간선 개수
 
 	vector<Edge> edges;
+	long long table[MAX];
 
+	for (int i = 0; i < MAX;i++) {
+		table[i] = INF;
+	}
+
+	table[1] = 0;
+
+	cin >> N >> M;
 	for (int i = 0; i < M; i++) {
 		int u, v, w;
 		cin >> u >> v >> w;
 		edges.push_back({ u,v,w });
 	}
 
-	long long table[MAX];
-
-	for (int i = 0; i < MAX; i++) {
-		table[i] = INF;
-	}
-
-	table[1] = 0; // start_point = 1번
-
-	for (int i = 1; i <= N - 1; i++) {
-		for (const auto& edge : edges) {
+	for (int i = 1; i <= N; i++) {
+		for (auto const& edge : edges) {
 			int u = edge.from;
 			int v = edge.to;
-			int weight = edge.cost;
+			int w = edge.cost;
 
-			if (table[u] != INF && table[v] > table[u] + weight) {
-				table[v] = table[u] + weight;
+			if (table[u] != INF && table[v] > table[u] + w) {
+				table[v] = table[u] + w;
 			}
 		}
 	}
 
-	bool minuscycle = false;
+	bool minus_chain = false;
 
-	for (const auto& edge : edges) {
+	for (auto const& edge : edges) {
 		int u = edge.from;
 		int v = edge.to;
-		int weight = edge.cost;
+		int w = edge.cost;
 
-		if (table[u] != INF && table[v] > table[u] + weight) {
-			minuscycle = true;
+		if (table[u] != INF && table[v] > table[u] + w) {
+			minus_chain = true;
 			break;
 		}
 	}
 
-	if (minuscycle) {
-		cout << -1 << "\n";
-	}
-	else {
-		for (int i = 2; i <= N; i++) {
+	for (int i = 2; i <= N; i++) {
+		if (minus_chain) {
+			cout << -1;
+			break;
+		}
+		else {
 			if (table[i] == INF) {
 				cout << -1 << "\n";
 			}
